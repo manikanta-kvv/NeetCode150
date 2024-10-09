4cbe96abda6b92cad6c3f85b27b2c344eba127b2
manikanta-kvv <manikantakvv6@gmail.com>
2024-10-10 00:37:59 +0530

    added approach 2

--- arrays_hashing/ContainsDuplicate.java
+++ arrays_hashing/ContainsDuplicate.java
@@ -1,11 +1,13 @@
 package arrays_hashing;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ContainsDuplicate {
 
-    public static boolean containsDuplicate(int[] nums) {
+    //approach 1
+    public static boolean containsDuplicate_ap1(int[] nums) {
         Map<Integer, Integer> hashMap = new HashMap<>();
         for(int i = 0; i < nums.length; i++) {
             if(hashMap.containsKey(nums[i])) {
@@ -16,9 +18,18 @@ public class ContainsDuplicate {
         return  false;
     }
 
+    //approach 2
+    public static boolean containsDuplicates_ap2(int[] nums) {
+        Arrays.sort(nums);
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] == nums[i-1]) return  true;
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 1};
-        boolean result = containsDuplicate(arr);
+        boolean result = containsDuplicate_ap1(arr);
         System.out.println(result);
     }
 
